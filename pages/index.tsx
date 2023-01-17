99d81f8b16c1e64c863c6014bbab967071951c36
Jan Kaifer <jan@kaifer.cz>
2023-01-17 16:31:29 +0100

    repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const [error, setError] = useState(false);
+  useEffect(() => setError(true), []);
+  if (error) {
+    throw new Error("Test error");
+  }
+  return null;
 }
